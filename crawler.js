c2235a791ee7aa2a8f7989d75dedb028be897ae8
Islam Yakout <117403825+c0depirate69@users.noreply.github.com>
2025-04-12 22:42:07 +0200

    Update crawler.js

--- crawler.js
+++ crawler.js
@@ -4,7 +4,10 @@ const crawl = require('./modules/crawl');
 const sop = require('./modules/sop');
 const postMessage = require('./modules/postmessage');
 const cors = require('./modules/cors');
-
+const browser = await puppeteer.launch({
+  executablePath: '/usr/bin/chromium',
+  headless: 'new' // opt in to the new headless mode
+});
 (async () => {
   const domains = fs.readFileSync('domains.txt', 'utf-8').split(/\r?\n/);
   for (const domain of domains) {
