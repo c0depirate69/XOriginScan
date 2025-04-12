ea278659787fb70895a50e6d860ff450089b8550
Islam Yakout <117403825+c0depirate69@users.noreply.github.com>
2025-04-12 22:26:52 +0200

    Create crawler.js

---
+++ crawler.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const crawl = require('./modules/crawl');
+const sop = require('./modules/sop');
+const postMessage = require('./modules/postmessage');
+const cors = require('./modules/cors');
+
+(async () => {
+  const domains = fs.readFileSync('domains.txt', 'utf-8').split(/\r?\n/);
+  for (const domain of domains) {
+    const origin = `http://${domain}`;
+    console.log(`\n\u001b[1m[*] Crawling: ${origin}\u001b[0m`);
+    await crawl(origin);
+    await sop(origin);
+    await postMessage(origin);
+    await cors(origin);
+  }
+})();
